239122fd94215156eacdf4d3b43e31cf2f94c12a
Pavel Raiskup <praiskup@redhat.com>
2019-04-19 23:39:08 +0200

    forgotten version bump

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ Resource allocator spawner/terminator scripts for OpenStack
 
 setup(
     name=project,
-    version='3',
+    version='4',
     description='Spawning/terminating scripts for openstack',
     long_description=long_description,
     author='Pavel Raiskup',
