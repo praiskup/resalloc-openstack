28780d1af3050c37c0486a17db44fd552d0dfad9
Pavel Raiskup <praiskup@redhat.com>
2024-02-06 17:10:13 +0100

    Release 9.7

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ Resource allocator spawner/terminator scripts for OpenStack
 
 setup(
     name=project,
-    version='9.6',
+    version='9.7',
     description='Spawning/terminating scripts for openstack',
     long_description=long_description,
     author='Pavel Raiskup',
