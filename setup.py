2c513e57458fac24b8652b1ac0fb78a2aa912cd2
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2026-04-23 11:41:39 +0200

    Release v9.10

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ Resource allocator spawner/terminator scripts for OpenStack
 
 setup(
     name=project,
-    version='9.9',
+    version='9.10',
     description='Spawning/terminating scripts for openstack',
     long_description=long_description,
     author='Pavel Raiskup',
